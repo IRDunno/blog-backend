0852c40cd78ff2daa82804730fbc36d328a2e7a6
Andre <glophics.andre@gmail.com>
2024-10-25 15:31:57 +0800

    fix: /auth/user route user model JWTSubject

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,6 @@ Route::group(["prefix" => "auth"], function() {
     Route::post("/register", [AuthController::class, "register"]);
     Route::post("/login", [AuthController::class, "login"]);
     Route::post("/logout", [AuthController::class, "logout"])->middleware(["auth:api"]);
+    Route::get("/user", [AuthController::class, "user"]);
 });
 
-Route::get("/user", [AuthController::class, "user"]);
