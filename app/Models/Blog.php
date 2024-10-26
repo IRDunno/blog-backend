7d934a49e9695bf0040cff89bfaf0c7785651773
aby calago <calagoaby@gmail.com>
2024-10-26 23:32:58 +0800

    feat: add blog_likes migration and models functions

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -5,13 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Blog extends Model
-{
-    use HasFactory;
+class Blog extends Model {
+  use HasFactory;
 
-    protected $guarded = ["id", "created_at", "updated_at"];
+  protected $guarded = ["id", "created_at", "updated_at"];
 
-    public function user() {
-        return $this->belongsTo(User::class);
-    }
+  public function user() {
+    return $this->belongsTo(User::class);
+  }
+
+  public function likes() {
+    return $this->belongsToMany(User::class, "blog_likes")->withTimestamps();
+  }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,57 +9,58 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable implements JWTSubject
-{
-    use HasApiTokens, HasFactory, Notifiable;
+class User extends Authenticatable implements JWTSubject {
+  use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'name',
+    'email',
+    'password',
+  ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array<int, string>
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array<string, string>
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+    'password' => 'hashed',
+  ];
 
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
+  public function getJWTIdentifier() {
+    return $this->getKey();
+  }
 
-    /**
-     * Return a key value array, containing any custom claims to be added to the JWT.
-     *
-     * @return array
-     */
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
+  /**
+   * Return a key value array, containing any custom claims to be added to the JWT.
+   *
+   * @return array
+   */
+  public function getJWTCustomClaims() {
+    return [];
+  }
 
-    public function blogs() {
-        return $this->hasMany(Blogs::class);
-    }
+  public function blogs() {
+    return $this->hasMany(Blog::class);
+  }
+  
+  public function likedBlogs() {
+    return $this->belongsToMany(Blog::class, "blog_likes")->withTimestamps();
+  }
 }
